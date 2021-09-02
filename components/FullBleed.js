20273406c25b2df61c6dc6bc88528559cb8c448c
Mattie Lee <57819411+mattiel@users.noreply.github.com>
2021-09-02 13:56:56 -0700

    Added brute option

--- components/FullBleed.js
+++ components/FullBleed.js
@@ -1,6 +1,6 @@
-const FullBleed = ({ children, className }) => {
+const FullBleed = ({ children, brute, className }) => {
   return(
-    <section className={`full-bleed ${className ? className : ''}`}>
+    <section className={`full-bleed${brute && ' full-bleed--brute'} ${className ? ' ' + className : ''}`}>
       {children}
     </section>
   )
