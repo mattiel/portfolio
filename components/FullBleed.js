2ea5400616008e3d732635bf4409fd7ebc498865
Mattie Lee <57819411+mattiel@users.noreply.github.com>
2021-09-14 16:52:10 -0700

    Clsx for clearer class names

--- components/FullBleed.js
+++ components/FullBleed.js
@@ -1,6 +1,15 @@
+import clsx from "clsx"
+
 const FullBleed = ({ children, brute, className }) => {
+
+  const classes = clsx(
+    "full-bleed",
+    brute && "full-bleed--brute",
+    className
+  )
+
   return(
-    <section className={`full-bleed${brute && ' full-bleed--brute'} ${className ? ' ' + className : ''}`}>
+    <section className={classes}>
       {children}
     </section>
   )
