44cdb2791d729a8685f559b9b688fbf7ea06b9dc
Mattie Lee <57819411+mattiel@users.noreply.github.com>
2021-09-27 22:31:25 -0700

    added toc prop

--- components/WorkInProgress.js
+++ components/WorkInProgress.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
 const WorkInProgress = ({ isAbsolute }) => {
   const classNames = clsx(
-    "inline-flex font-medium items-center py-2 px-3 bottom-6 right-6 rounded-lg bg-black bg-opacity-80 text-white",
+    "inline-flex font-medium items-center py-2 px-3 bottom-6 right-6 rounded-lg bg-black bg-opacity-70 backdrop-blur-lg text-white z-20",
     isAbsolute ? "absolute" : "fixed"
-  )
+  );
 
   return (
-    <div 
+    <div
       className={classNames}
       style={{
-        gridColumn: 'unset'
+        gridColumn: "unset",
       }}
     >
       <div className="grid place-items-center w-4 h-4 mr-2">
-        <div className="pulsing-blob"></div>
+        <div className="pulsing-blob"></div>]
       </div>
       Work In Progress
     </div>
