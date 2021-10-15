74699acafabe49088a106d28d63d089631cd214a
Mattie Lee <hello@mattiel.dev>
2021-10-14 20:12:54 -0700

    Layout dark mode added for nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,20 @@
-import Navbar from '@/components/Navbar'
-import Container from '@/components/Container'
-import Footer from '@/components/Footer'
+import Navbar from "@/components/Navbar";
+import Container from "@/components/Container";
+import Footer from "@/components/Footer";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, dark }) => {
   return (
     <div className="layout">
-      <div 
+      <div
         className="absolute -left-5 w-full max-w-screen-xl top-0 h-full mx-5 border-l border-r border-dashed border-gray-500 border-opacity-10 pointer-events-none flex justify-evenly items-start"
-        style={{zIndex: -1}}
+        style={{ zIndex: -1 }}
       >
-        <div className="ml-2 h-full border-l border-dashed border-gray-500 border-opacity-10 hidden md:block" style={{width: '1px'}}></div>
+        <div
+          className="ml-2 h-full border-l border-dashed border-gray-500 border-opacity-10 hidden md:block"
+          style={{ width: "1px" }}
+        ></div>
       </div>
-      <Navbar />
+      <Navbar dark={dark} />
       {children}
       <Footer />
     </div>
