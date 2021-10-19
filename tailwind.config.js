5fefe34858dc8a3f4eb679ce4438e588a2683eca
Mattie Lee <hello@mattiel.dev>
2021-10-18 18:48:13 -0700

    Added color configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.blue,
+      yellow: colors.yellow,
+      green: colors.green,
+      orange: colors.orange,
+      purple: colors.purple,
+      pink: colors.pink,
+      white: colors.white,
+      black: colors.black,
+    }
   },
   variants: {
     extend: {},
