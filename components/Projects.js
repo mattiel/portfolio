0e28b4f58f6ceafd94bbcc456664e82fc539a76d
Mattie Lee <hello@mattiel.dev>
2021-11-23 22:06:44 -0800

    Updated projects styling

--- components/Projects.js
+++ components/Projects.js
@@ -11,16 +11,24 @@ const Projects = ({ manifest }) => {
               return (
                 <Link href={`/projects/${value.title.toLowerCase()}`} key={idx}>
                   <div 
-                    className="cursor-pointer overflow-hidden flex flex-col"
+                    className="cursor-pointer overflow-hidden flex justify-between flex-col border border-gray-100 rounded-3xl border-gray-100 bg-white transform hover:scale-105 hover:shadow-2xl duration-300 transition transition-all"
                   >
+                    <figcaption className="flex flex-col space-y-1 p-12">
+                      <h3 className="font-semibold text-gray-700">{value.title}</h3>
+                      {/* <h4 className="text-md font-semibold">{value.subtitle}</h4> */}
+                      {/* {idx === 0 &&
+                        <p className="lg:text-xl font-medium">{value.description}</p>
+                      } */}
+                      <p className="lg:text-xl font-medium text-gray-900">{value.description}</p>
+                    </figcaption>
                     <figure 
-                      className="w-full mb-4 overflow-hidden relative"
+                      className="w-full overflow-hidden relative"
                       style={{ backgroundColor: `${value.color}` }}
                     > 
                       <div 
                         className={`
-                        bg-gray-100 bg-opacity-95 w-full px-6 py-8 bg-no-repeat transition duration-300 hover:scale-105 bg-full-cover
-                          lg:h-[30rem] h-96 items-start bg-cover bg-center
+                        bg-gray-100 bg-opacity-95 w-full px-6 py-8 bg-no-repeat bg-full-cover
+                          lg:h-[26rem] h-96 items-start bg-cover bg-center overflow-hidden
                         `}
                         style={{
                           backgroundImage: `url('${value.thumbnail}')`
@@ -31,14 +39,6 @@ const Projects = ({ manifest }) => {
                         <WorkInProgress isAbsolute/>
                       )}
                     </figure>
-                    <figcaption className="flex flex-col space-y-1 mb-6">
-                      <h3 className="font-semibold text-gray-700">{value.title}</h3>
-                      {/* <h4 className="text-md font-semibold">{value.subtitle}</h4> */}
-                      {/* {idx === 0 && 
-                        <p className="lg:text-xl font-medium">{value.description}</p>
-                      } */}
-                      <p className="lg:text-xl font-medium text-gray-900">{value.description}</p>
-                    </figcaption>
                   </div>
                 </Link>
               )
