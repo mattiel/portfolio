42d54e95d70a891252666014306732448e71f355
Mattie Lee <hello@mattiel.dev>
2021-11-23 22:05:31 -0800

    Added head

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,19 @@
 import Navbar from "@/components/Navbar";
-import Container from "@/components/Container";
+import Head from "next/head";
 import Footer from "@/components/Footer";
 
-const Layout = ({ children, dark }) => {
+const Layout = ({ children, dark, title }) => {
   return (
     <div className="layout">
+      <Head>
+        <title>{title ? title : "Mattie Lee"}</title>
+        <meta property="og:title" content={title ? title : "Mattie Lee"} key="title" />
+        <meta
+          name="theme-color"
+          content={dark ? "#171717" : "#2563eb"}
+          key="theme-color"
+        />
+      </Head>
       <div
         className="absolute -left-5 w-full max-w-screen-xl top-0 h-full mx-5 border-l border-r border-dashed border-gray-500 border-opacity-10 pointer-events-none flex justify-evenly items-start"
         style={{ zIndex: -1 }}
