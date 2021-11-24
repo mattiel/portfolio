bbe5cdc88edf8c2c32bd571b38a58aa899745618
Mattie Lee <hello@mattiel.dev>
2021-11-23 22:05:57 -0800

    Refined sizing

--- components/Phone.js
+++ components/Phone.js
@@ -3,26 +3,25 @@ import clsx from 'clsx';
 
 const Phone = ({ source, maxWidth, className, isVideo }) => {
   const outerClasses = clsx(
-    'rounded-[3rem] bg-black border-gray-600 border-4 inline-block drop-shadow-2xl',
+    'rounded-[3rem] bg-black border-gray-600 border-4 inline-block drop-shadow-2xl max-w-[320px]',
     className,
   )
 
   const innerClasses = clsx(
-    'w-full h-full max-w-[23.4375rem] max-h-[50.75rem] bg-black',
-    maxWidth ? maxWidth : 'max-w-[23.4375rem]',
+    'bg-black',
   )
 
   return (
     <div 
       className={outerClasses}
       style={{
-        borderWidth: "clamp(0.125rem, .5vw, .35rem)"
+        borderWidth: "clamp(0.125rem, 10vw, .35rem)"
       }}
     >
       <div 
         className="border-[clamp(.75rem)] border-black rounded-[3rem] webkit-fix-overflow overflow-hidden"
         style={{
-          borderWidth: "clamp(.25rem, 1vw, .75rem)"
+          borderWidth: "clamp(.25rem, 10vw, .75rem)"
         }}
       >
         <div className={innerClasses}>
