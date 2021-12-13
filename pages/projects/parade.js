29358a27bddb0fe0d476f97f32692f7060ba8463
Mattie Lee <hello@mattiel.dev>
2021-12-13 11:05:11 -0800

    Parade page update

--- pages/projects/parade.js
+++ pages/projects/parade.js
@@ -3,9 +3,13 @@ import Layout from "@/components/Layout";
 const Parade = () => {
   return (
     <Layout>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 mt-24 gap-12">
-        <img src="/parade/poster.webp"/>
-        <div className="place-self-end py-4">
+      <div className="mt-24">
+        <video className="w-full" width="768" height="1440" autoPlay controls>
+          <source src="/parade/promo.mp4" type="video/mp4"/>
+        </video>
+      </div>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 mt-12 gap-12">
+        <div className="place-self-end">
           <h2 className="font-bold text-4xl mb-4">Club Euphoria</h2>
           <p className="mb-8">
             Euphoria repaints an undiscovered/overlooked DTLA alley into multiple “stages” -- creating multiple festive
@@ -14,22 +18,22 @@ const Parade = () => {
             dance music (EDM) to hardcore techno music by overlaying and placing effects and sounds on the scenes as
             they take a few steps through the linear pathway.
           </p>
-          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div>
-              <h3 className="font-bold text-xl mb-2">Location</h3>
-              <p className="mb-4">
-                2042 Sacramento St
-              </p>
-            </div>
-            <div>
-              <h3 className="font-bold text-xl mb-2">When</h3>
-              <p className="mb-4">
-                Dec 13. 2021 - April 2021
-              </p>
-            </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="w-full">
+            <h3 className="font-bold text-xl mb-2">Location</h3>
+            <p className="mb-4">
+              2042 Sacramento St
+            </p>
+          </div>
+          <div className="w-full">
+            <h3 className="font-bold text-xl mb-2">When</h3>
+            <p className="mb-4">
+              Dec 13. 2021 - April 2021
+            </p>
+          </div>
         </div>
       </div>
-    </div>
 
     <div className="py-32">
       <h2 className="font-bold text-4xl mb-4">Prototypes</h2>
