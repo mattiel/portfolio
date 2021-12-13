26638348f56b102f1409af79791962c15e0bcf9b
Mattie Lee <hello@mattiel.dev>
2021-12-13 10:16:12 -0800

    Parade page update

--- components/Contact.js
+++ components/Contact.js
@@ -1,7 +1,9 @@
+import Quill from "@/components/Quill";
+
 const Contact = () => {
   return (
     <div className="py-24">
-      <div className="border border-gray-200 bg-white rounded-xl p-8 md:p-12  grid grid-cols-1 md:grid-cols-2 gap-4 place-items-start">
+      <div className="border border-gray-200 bg-white rounded-xl p-12 gap-12 md:p-12 grid grid-cols-1 md:grid-cols-2 gap-4 place-items-start">
         <div className="grid gap-2">
           <h3 className="text-4xl font-bold text-gray-900">
             Let's talk
@@ -10,19 +12,22 @@ const Contact = () => {
             I'm happy to discuss more about this project or future opportunities. Feel free to reach out to me.
           </p>
         </div>
-        <a 
-          className="inline-flex items-center text-gray-700 text-xl border-b-4 border-gray-700 
-                hover:text-purple-600 hover:border-purple-600 transition-all md:place-self-end" 
-          href="mailto: hello@mattiel.dev">
-          hello@mattiel.dev
-          <svg 
-            className="w-5 h-5 ml-1" 
-            fill="none" stroke="currentColor" 
-            viewBox="0 0 24 24" 
-            xmlns="http://www.w3.org/2000/svg">
-            <path strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
-          </svg>
-        </a>
+        <div className="grid grid-cols-1 gap-6 place-items-start">
+          <Quill />
+          <a
+            className="inline-flex items-center text-gray-700 text-xl border-b-4 border-gray-700
+                hover:text-purple-600 hover:border-purple-600 transition-all"
+            href="mailto: hello@mattiel.dev">
+            hello@mattiel.dev
+            <svg
+              className="w-5 h-5 ml-1"
+              fill="none" stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path strokeWidth={2} d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   );
--- pages/projects/parade.js
+++ pages/projects/parade.js
@@ -3,20 +3,64 @@ import Layout from "@/components/Layout";
 const Parade = () => {
   return (
     <Layout>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 mt-24 gap-8">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 mt-24 gap-12">
         <img src="/parade/poster.webp"/>
-        <div className="place-self-center">
-          <h2 className="font-bold text-4xl mb-4">Euphoria</h2>
-          <p className="">
+        <div className="place-self-end py-4">
+          <h2 className="font-bold text-4xl mb-4">Club Euphoria</h2>
+          <p className="mb-8">
             Euphoria repaints an undiscovered/overlooked DTLA alley into multiple “stages” -- creating multiple festive
             and rave experiences through multiple stages along the pathway. This AR experience encourages attendees to
-            embrace the abandoned areas through euphoric experience.
-
-            The stages vary from chill and mellow electronic dance music (EDM) to hardcore techno music by converting
-            scenes around them as they take a few steps through the pathway.
+            embrace the abandoned areas through euphoric experience. The stages vary from chill and mellow electronic
+            dance music (EDM) to hardcore techno music by overlaying and placing effects and sounds on the scenes as
+            they take a few steps through the linear pathway.
           </p>
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div>
+              <h3 className="font-bold text-xl mb-2">Location</h3>
+              <p className="mb-4">
+                2042 Sacramento St
+              </p>
+            </div>
+            <div>
+              <h3 className="font-bold text-xl mb-2">When</h3>
+              <p className="mb-4">
+                Dec 13. 2021 - April 2021
+              </p>
+            </div>
         </div>
       </div>
+    </div>
+
+    <div className="py-32">
+      <h2 className="font-bold text-4xl mb-4">Prototypes</h2>
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-24">
+        <div>
+          <h3 className="font-bold text-xl mb-2">Entrance</h3>
+          <video className="rounded-3xl mb-8" width="768" height="1440" autoPlay loop>
+            <source src="/parade/stage1.mp4" type="video/mp4"/>
+          </video>
+          <h3 className="font-bold text-xl mb-2">Scan to try on Snapchat</h3>
+          <img src="/parade/code1.svg" className="max-w-[12rem]"/>
+        </div>
+        <div>
+          <h3 className="font-bold text-xl mb-2">Mid-way</h3>
+          <video className="rounded-3xl mb-8" width="768" height="1440" autoPlay loop>
+            <source src="/parade/stage2.mp4" type="video/mp4"/>
+          </video>
+          <h3 className="font-bold text-xl mb-2">Scan to try on Snapchat</h3>
+          <img src="/parade/code2.svg" className="max-w-[12rem]"/>
+
+        </div>
+        <div>
+          <h3 className="font-bold text-xl mb-2">End</h3>
+          <video className="rounded-3xl mb-8" width="768" height="1440" autoPlay loop>
+            <source src="/parade/stage3.mp4" type="video/mp4"/>
+          </video>
+          <h3 className="font-bold text-xl mb-2">Scan to try on Snapchat</h3>
+          <img src="/parade/code3.svg" className="max-w-[12rem]"/>
+        </div>
+      </div>
+    </div>
     </Layout>
   );
 };
